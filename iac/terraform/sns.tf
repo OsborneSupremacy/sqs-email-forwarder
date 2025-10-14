82f6cb75783ee375c2b1f89773c8c8310f19da7c
Ben Osborne <ben@osbornesupremacy.com>
2025-10-14 15:01:08 -0500

    created other SNS topic

--- iac/terraform/sns.tf
+++ iac/terraform/sns.tf
@@ -2,6 +2,10 @@ resource "aws_sns_topic" "silverconcord_inbox" {
   name = "sns-topic-silverconcord-inbox"
 }
 
+resource "aws_sns_topic" "osbornesupremancy_inbox" {
+  name = "sns-topic-osbornesupremancy-inbox"
+}
+
 resource "aws_sns_topic_policy" "silverconcord_inbox_policy" {
   arn = aws_sns_topic.silverconcord_inbox.arn
 
@@ -16,6 +20,33 @@ resource "aws_sns_topic_policy" "silverconcord_inbox_policy" {
         }
         Action   = "SNS:Publish"
         Resource = aws_sns_topic.silverconcord_inbox.arn
+        Condition = {
+          StringEquals = {
+            "AWS:SourceAccount" = "182571449491"
+          }
+          StringLike = {
+            "AWS:SourceArn" = "arn:aws:ses:*"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_sns_topic_policy" "osbornesupremancy_inbox_policy" {
+  arn = aws_sns_topic.osbornesupremancy_inbox.arn
+
+  policy = jsonencode({
+    Version = "2008-10-17"
+    Statement = [
+      {
+        Sid    = "stmt1758403689843"
+        Effect = "Allow"
+        Principal = {
+          Service = "ses.amazonaws.com"
+        }
+        Action   = "SNS:Publish"
+        Resource = aws_sns_topic.osbornesupremancy_inbox.arn
         Condition = {
           StringEquals = {
             "AWS:SourceAccount" = "182571449491"
